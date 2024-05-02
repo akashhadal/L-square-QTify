ce8ed6fe2375ef400666917cfa1b6187a3f389e4
Akash Hadal <akash.hadal@gmail.com>
2024-05-02 23:30:22 +0530

    ME_QTIFY_V1_ALBUM_1.0

--- qtify/src/components/Carousel/CarouselRight/CarouselRight.js
+++ qtify/src/components/Carousel/CarouselRight/CarouselRight.js
@@ -1,27 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import {ReactComponent as RightArrow} from "../../../assets/RightArrow.svg";
-import { useSwiper, Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import styles from "./CarouselRight.module.css";
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useSwiper } from 'swiper/react'
+import {ReactComponent as RightArrow} from '../../../assets/rightArrow.svg'
+import styles from './CarouselRight.module.css'
 
+const CarouselRightNavigation = () => {
+    const swiper = useSwiper();
+    const [isEnd,setIsEnd] = useState(swiper.isEnd)
 
-const CarouselRight = () => {
-  let swiper =useSwiper();
-  const[isEnd, setIsEnd]=useState(swiper.isEnd);
-
-  useEffect(()=>{
-    swiper.on("slideChange", () => {
-      // to set the state when ever we change the slide, to control conditional rendering of Right arrow button
-      setIsEnd(swiper.isEnd)
-  })
-  },[swiper]);
-
+    useEffect(() => {
+        swiper.on("slideChange", () => {
+            setIsEnd(swiper.isEnd)
+        })
+    })
   return (
     <div className={styles.rightNavigation}>
-      {/* don't show Right arrow when we are on the Right most card */}
-      {!isEnd && <RightArrow onClick={()=>swiper.slideNext()}/>}
+        {!isEnd && <RightArrow onClick={() => swiper.slideNext()}/>}
     </div>
   )
 }
 
-export default CarouselRight;
+export default CarouselRightNavigation
